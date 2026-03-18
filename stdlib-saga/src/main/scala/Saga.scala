ec07bc75c305ff3ba65ed4744330549f573f3efc
ichi-h <85932406+ichi-h@users.noreply.github.com>
2026-03-18 14:35:40 +0900

    chore: run format.

--- stdlib-saga/src/main/scala/Saga.scala
+++ stdlib-saga/src/main/scala/Saga.scala
@@ -5,7 +5,7 @@ type Compensation = SagaState => Future[Either[String, SagaState]]
 case class SagaState(steps: Vector[String], compensations: Vector[Compensation]):
   def addStep(step: String): SagaState               = copy(steps = steps :+ step)
   def addCompensation(comp: Compensation): SagaState = copy(compensations = compensations :+ comp)
-  def stepsSummary: String = steps.map(s => s"- $s").mkString("\n")
+  def stepsSummary: String                           = steps.map(s => s"- $s").mkString("\n")
 
 object SagaState:
   def empty: SagaState = SagaState(Vector.empty, Vector.empty)
@@ -18,7 +18,7 @@ case class Saga[A](run: SagaState => Future[(SagaState, Either[String, A])]):
   def flatMap[B](f: A => Saga[B])(using ExecutionContext): Saga[B] =
     Saga { state =>
       run(state).flatMap {
-        case (s1, Right(a)) => f(a).run(s1)
+        case (s1, Right(a))  => f(a).run(s1)
         case (s1, Left(err)) => Future.successful((s1, Left(err)))
       }
     }
@@ -26,7 +26,7 @@ case class Saga[A](run: SagaState => Future[(SagaState, Either[String, A])]):
   def withCompensation(undo: Compensation)(using ExecutionContext): Saga[A] =
     Saga { state =>
       run(state).map {
-        case (s1, Right(a)) => (s1.addCompensation(undo), Right(a))
+        case (s1, Right(a))  => (s1.addCompensation(undo), Right(a))
         case (s1, Left(err)) => (s1, Left(err))
       }
     }
@@ -59,7 +59,7 @@ object Saga:
   )(using ExecutionContext): Saga[A] =
     val runStep: Saga[A] = Saga { state =>
       op().map {
-        case Left(err) => (state.addStep(s"failed: $name"), Left(s"$name failed: $err"))
+        case Left(err)     => (state.addStep(s"failed: $name"), Left(s"$name failed: $err"))
         case Right(result) => (state.addStep(s"success: $name"), Right(result))
       }
     }
